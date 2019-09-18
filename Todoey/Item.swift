1a8ef7c2159c39fdc9648665e1ce19747c9b3e22
Letitia Wu <Letitia_wood@icloud.com>
2019-09-18 11:17:27 +0800

    model added

---
+++ Todoey/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Letitia Wu on 2019/9/18.
+//  Copyright © 2019 Letitia Wu. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
